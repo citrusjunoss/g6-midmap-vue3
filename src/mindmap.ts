ce87ec9e10385bb5b63f5ed34710ad01adedc60b
橙 <chengjunchao@hotmail.com>
2024-09-13 16:42:45 +0800

    feat: remove addNode

--- src/mindmap.ts
+++ src/mindmap.ts
@@ -215,57 +215,6 @@ class MindmapNode extends BaseNode {
     });
   }
 
-  getAddStyle(attributes: any) {
-    const { collapsed, showIcon, direction } = attributes;
-    if (collapsed || !showIcon) return false;
-    const [width, height] = this.getSize(attributes);
-    const color = '#ddd';
-
-    const offsetX = this.isShowCollapse(attributes) ? 24 : 12;
-    const isRoot = this.id === this.rootId;
-
-    return {
-      backgroundFill: '#fff',
-      backgroundHeight: 12,
-      backgroundLineWidth: 1,
-      backgroundStroke: color,
-      backgroundWidth: 12,
-      cursor: 'pointer',
-      fill: color,
-      fontFamily: 'iconfont',
-      fontSize: 8,
-      text: '\ue664',
-      textAlign: 'center',
-      x: isRoot ? width + 12 : direction === 'left' ? -offsetX : width + offsetX,
-      y: isRoot ? height / 2 : height,
-    };
-  }
-
-  getAddBarStyle(attributes: any) {
-    const { collapsed, showIcon, direction, color = COLORS[0] } = attributes;
-    if (collapsed || !showIcon) return false;
-    const [width, height] = this.getSize(attributes);
-
-    const offsetX = this.isShowCollapse(attributes) ? 12 : 0;
-    const isRoot = this.id === this.rootId;
-
-    const HEIGHT = 2;
-    const WIDTH = 6;
-
-    return {
-      cursor: 'pointer',
-      fill:
-        direction === 'left'
-          ? `linear-gradient(180deg, #fff 20%, ${color})`
-          : `linear-gradient(0deg, #fff 20%, ${color})`,
-      height: HEIGHT,
-      width: WIDTH,
-      x: isRoot ? width : direction === 'left' ? -offsetX - WIDTH : width + offsetX,
-      y: isRoot ? height / 2 - HEIGHT / 2 : height - HEIGHT / 2,
-      zIndex: -1,
-    };
-  }
-
 
   forwardEvent(target: any, type: any, listener: any) {
     if (target && !Reflect.has(target, '__bind__')) {
